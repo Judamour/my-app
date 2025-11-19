759b41d71bbbf43c4d52086102f4725f3fa3d5f6
judamour <code.judamour@gmail.com>
2025-11-19 11:11:34 +0100

    modification des roles pour n'avoir que user et admin

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,24 +1,36 @@
-import { UserRole } from '@prisma/client'
-import NextAuth from 'next-auth'
+import { DefaultSession } from 'next-auth'
 
 declare module 'next-auth' {
-  interface User {
-    role: UserRole
-  }
-  
   interface Session {
     user: {
       id: string
       email: string
-      name: string
+      firstName: string
+      lastName: string
       role: string
-    }
+      isOwner: boolean
+      isTenant: boolean
+    } & DefaultSession['user']
+  }
+
+  interface User {
+    id: string
+    email: string
+    firstName: string
+    lastName: string
+    role: string
+    isOwner: boolean
+    isTenant: boolean
   }
 }
 
 declare module 'next-auth/jwt' {
   interface JWT {
-    role: string
     id: string
+    role: string
+    isOwner: boolean
+    isTenant: boolean
+    firstName: string
+    lastName: string
   }
 }
